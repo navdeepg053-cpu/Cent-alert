df8f340ed57a5728e49ac3521a4b72a8b51dbf7a
navdeepg053-cpu <navdeepg053@gmail.com>
2026-02-09 20:09:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,15 @@
-import os
-from telegram.ext import Application
+import telebot
 
-TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")   # or os.getenv("TELEGRAM_BOT_TOKEN")
-application = Application.builder().token(TOKEN).build()
+BOT_TOKEN = "8327314498:AAHtqV2n9TQzhjj0UTgYNHqdMRbHuCvkKeg"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'id'])
+def send_id(message):
+    bot.reply_to(message, f"Your Chat ID:\n{message.chat.id}")
+
+@bot.message_handler(func=lambda m: True)
+def echo(message):
+    bot.reply_to(message, f"Chat ID: {message.chat.id}")
+
+print("Bot starting...")
+bot.polling()
